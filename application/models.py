92b4332f8ca06d7e1cabab657aa61f10ede245c3
JayRowlands <56541131+JayRowlands@users.noreply.github.com>
2021-11-10 12:46:23 +0000

    Created models

---
+++ application/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import relationship
+from application import db
+
+class Employees(db.model):
+    employee_id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(45))
+    role = db.Column(db.String(45))
+    email = db.Column(db.String(45))
+    mobile_num = db.Column(db.Integer)
+    orders = relationship(lambda: Orders)
+
+class Orders(db.model):
+    order_id = db.Column(db.Integer, primary_key = True)
+    order_name = db.Column(db.String(45))
+    cost = db.Column(db.Integer)
+    cust_name = db.Column(db.String(45))
+    parent_id = db.Column(db.Integer, db.ForeignKey(Employees.employee_id))

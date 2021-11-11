74d8a4439fe8875c192a5b3c9bc4190e329aaadd
JayRowlands <56541131+JayRowlands@users.noreply.github.com>
2021-11-11 16:18:28 +0000

    Fixed error where integer truncates leading 0's

--- application/forms.py
+++ application/forms.py
@@ -7,14 +7,14 @@ class AddEmployee(FlaskForm):
     name = StringField("Name")
     role = SelectField("Role", choices = [('Breakfast Chef', 'Breakfast Chef'), ('Starter Chef', 'Starter Chef'), ('main_chef', 'Main Chef'), ('Dessert Chef', 'Dessert Chef')])
     email = StringField("Email")
-    mobile_num = IntegerField("Mobile Number")
+    mobile_num = StringField("Mobile Number")
     submit = SubmitField("Add Employee")
 
 class UpdateEmployee(FlaskForm):
     name = StringField("Name")
     role = SelectField("Role", choices = [('breakfast_chef', 'Breakfast Chef'), ('starter_chef', 'Starter Chef'), ('main_chef', 'Main Chef'), ('dessert_chef', 'Dessert Chef')])
     email = StringField("Email")
-    mobile_num = IntegerField("Mobile Number")
+    mobile_num = StringField("Mobile Number")
     submit = SubmitField("Update Employee")
 
 class AddOrder(FlaskForm):

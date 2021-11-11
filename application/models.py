ee6137b5d8eff5db2c2fcda462a32ad4a91ad6a1
JayRowlands <56541131+JayRowlands@users.noreply.github.com>
2021-11-11 11:18:45 +0000

    Changed typo

--- application/models.py
+++ application/models.py
@@ -1,13 +1,13 @@
 from application import db
 
-class Employees(db.model):
+class Employees(db.Model):
     employee_id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(45))
     role = db.Column(db.String(45))
     email = db.Column(db.String(45))
     mobile_num = db.Column(db.Integer)
 
-class Orders(db.model):
+class Orders(db.Model):
     order_id = db.Column(db.Integer, primary_key = True)
     order_name = db.Column(db.String(45))
     cost = db.Column(db.Integer)
